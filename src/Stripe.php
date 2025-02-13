ac77e9fe9162dd33faf748b1186b934dfe076769
Marc Beinder <marc@marcbeinder.com>
2025-02-13 12:45:05 -0600

    Create Financial Connections Session

--- src/Stripe.php
+++ src/Stripe.php
@@ -9,6 +9,9 @@ namespace EncoreDigitalGroup\Common\Stripe;
 
 use EncoreDigitalGroup\Common\Stripe\Objects\Customer\StripeCustomer;
 use EncoreDigitalGroup\Common\Stripe\Support\HasStripe;
+use PHPGenesis\Logger\Logger;
+use Stripe\Exception\ApiErrorException;
+use Stripe\FinancialConnections\Session as FinancialConnectionsSession;
 
 class Stripe
 {
@@ -17,5 +20,22 @@ class Stripe
     public static function customer(mixed ...$params): StripeCustomer
     {
         return StripeCustomer::make(...$params);
+    }
+
+    public function createFinancialConnectionsSession(StripeCustomer $customer): ?FinancialConnectionsSession
+    {
+        try {
+            return $this
+                ->stripe
+                ->financialConnections
+                ->sessions
+                ->create($customer->toArray());
+        } catch (ApiErrorException $exception) {
+            Logger::error("[Stripe] Exception {exception.message}", [
+                "exception.message" => $exception->getMessage(),
+            ]);
+        }
+
+        return null;
     }
 }
