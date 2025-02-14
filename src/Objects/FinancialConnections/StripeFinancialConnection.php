5bf664a0920a1c04610237d79969e2c1cfc33a63
Marc Beinder <marc@marcbeinder.com>
2025-02-14 12:25:05 -0600

    Cleanup StripeFinancialConnection

--- src/Objects/FinancialConnections/StripeFinancialConnection.php
+++ src/Objects/FinancialConnections/StripeFinancialConnection.php
@@ -8,38 +8,16 @@ namespace EncoreDigitalGroup\Common\Stripe\Objects\FinancialConnections;
 
 use EncoreDigitalGroup\Common\Stripe\Objects\Customer\StripeCustomer;
 use EncoreDigitalGroup\Common\Stripe\Support\HasMake;
-use PHPGenesis\Logger\Logger;
-use Stripe\Exception\ApiErrorException;
-use Stripe\FinancialConnections\Session as FinancialConnectionsSession;
-use Stripe\StripeClient;
 
 class StripeFinancialConnection
 {
     use HasMake;
 
     public function __construct(
-        public StripeClient   $stripe,
         public StripeCustomer $customer,
         public array          $permissions = ["transactions"]
     )
     {
-    }
-
-    public function create(): ?FinancialConnectionsSession
-    {
-        try {
-            return $this
-                ->stripe
-                ->financialConnections
-                ->sessions
-                ->create($this->customer->toArray());
-        } catch (ApiErrorException $exception) {
-            Logger::error("[Stripe] Exception {exception.message}", [
-                "exception.message" => $exception->getMessage(),
-            ]);
-        }
-
-        return null;
     }
 
     public function toArray(): array
