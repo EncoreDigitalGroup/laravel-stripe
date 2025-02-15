ed89a53cdeb85b6cc724984e31a09302722cfc3f
EncoreBot <ghbot@encoredigitalgroup.com>
2025-02-15 23:39:00 +0000

    Dusting

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -12,9 +12,7 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    public function register(): void
-    {
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
