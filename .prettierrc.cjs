da66694a1a06aa0d79a3a63161815042ad0dcfd9
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-02-22 14:20:10 -0600

    Update TypeScript Config (#11)

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,17 @@
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Right Reserved.
+ */
+
 module.exports = {
-    ...require('gts/.prettierrc.json'),
-    plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
-    endOfLine: 'auto',
+    plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
+    bracketSpacing: false,
+    trailingComma: "all",
+    arrowParens: "avoid",
     tabWidth: 4,
     useTabs: false,
     printWidth: 120,
     importOrderSeparation: true,
+    singleQuote: false,
+    semi: true,
 };
