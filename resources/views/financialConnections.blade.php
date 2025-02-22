d6aeb2eb8e3da4117198708b17e6eac1ccb3110c
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-02-21 19:42:12 -0600

    Fix Stripe Views and StripeJS (#5)
    Co-authored-by: EncoreBot <ghbot@encoredigitalgroup.com>

--- resources/views/financialConnections.blade.php
+++ resources/views/financialConnections.blade.php
@@ -1,8 +1,10 @@
 <script>
-    const stripe = new Stripe("{{ $stripePublicKey }}")
+    const stripe = new Stripe("{{ $stripePublicKey }}");
     stripe.collectFinancialConnectionsAccounts({
         clientSecret: "{{ $stripeSessionSecret }}",
-    }).then(function (result) {
-        console.info(result);
-    });
+    }).then(function (event) {
+        window.location.href = "{{ $redirectSuccessUrl }}"
+    }).catch(function (event) {
+        window.location.href = "{{ $redirectErrorUrl }}"
+    })
 </script>
