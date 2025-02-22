223edf0cc66be4e9771e9f6a2647441e40a08ee1
Marc Beinder <marc@marcbeinder.com>
2025-02-22 14:54:56 -0600

    Reorganize TypeScript Classes

---
+++ src/ts/FinancialConnection.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+import {loadStripe} from "@stripe/stripe-js";
+
+export class FinancialConnection {
+    private readonly stripePublicKey: string;
+    private readonly stripeSessionSecret: string;
+    private readonly redirectSuccessUrl: string;
+    private readonly redirectErrorUrl: string;
+
+    constructor(
+        stripePublicKey: string,
+        stripeSessionSecret: string,
+        redirectSuccessUrl: string,
+        redirectErrorUrl: string,
+    ) {
+        this.stripePublicKey = stripePublicKey;
+        this.stripeSessionSecret = stripeSessionSecret;
+        this.redirectSuccessUrl = redirectSuccessUrl;
+        this.redirectErrorUrl = redirectErrorUrl;
+    }
+
+    async initialize(): Promise<void> {
+        const stripe = await loadStripe(this.stripePublicKey);
+        if (!stripe) {
+            throw new Error("Failed to initialize Stripe");
+        }
+
+        try {
+            await stripe.collectFinancialConnectionsAccounts({
+                clientSecret: this.stripeSessionSecret,
+            });
+            window.location.href = this.redirectSuccessUrl;
+        } catch (error) {
+            window.location.href = this.redirectErrorUrl;
+        }
+    }
+}
