426a942faea9501b6b445e04aec48ff44d0ba0e9
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-10-18 20:37:24 -0500

    Update Docs

--- src/php/Stripe.php
+++ src/php/Stripe.php
@@ -10,12 +10,6 @@ namespace EncoreDigitalGroup\Stripe;
 use EncoreDigitalGroup\Stripe\Objects\Customer\StripeCustomer;
 use EncoreDigitalGroup\Stripe\Objects\Subscription\StripeSubscription;
 use EncoreDigitalGroup\Stripe\Objects\Webhook\StripeWebhookEndpoint;
-use EncoreDigitalGroup\Stripe\Services\StripeCustomerService;
-use EncoreDigitalGroup\Stripe\Services\StripePriceService;
-use EncoreDigitalGroup\Stripe\Services\StripeProductService;
-use EncoreDigitalGroup\Stripe\Services\StripeSubscriptionScheduleService;
-use EncoreDigitalGroup\Stripe\Services\StripeSubscriptionService;
-use EncoreDigitalGroup\Stripe\Services\StripeWebhookEndpointService;
 use EncoreDigitalGroup\Stripe\Support\HasStripe;
 use EncoreDigitalGroup\Stripe\Support\Testing\FakeStripeClient;
 use Stripe\StripeClient;
@@ -39,40 +33,6 @@ class Stripe
     public static function webhook(): StripeWebhookEndpoint
     {
         return StripeWebhookEndpoint::make();
-    }
-
-    #endregion
-
-    #region Service Accessor Methods - Get service instances
-
-    public static function customers(): StripeCustomerService
-    {
-        return StripeCustomerService::make();
-    }
-
-    public static function products(): StripeProductService
-    {
-        return StripeProductService::make();
-    }
-
-    public static function prices(): StripePriceService
-    {
-        return StripePriceService::make();
-    }
-
-    public static function subscriptions(): StripeSubscriptionService
-    {
-        return StripeSubscriptionService::make();
-    }
-
-    public static function subscriptionSchedules(): StripeSubscriptionScheduleService
-    {
-        return StripeSubscriptionScheduleService::make();
-    }
-
-    public static function webhookEndpoints(): StripeWebhookEndpointService
-    {
-        return StripeWebhookEndpointService::make();
     }
 
     #endregion
